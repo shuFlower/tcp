08b86ef6721ec6ccc690057ba229f093e3bef2d9
shuFlower <1510276107@qq.com>
2015-08-18 08:19:00 +0800

    Update server.php
    psr,debug

--- server.php
+++ server.php
@@ -6,41 +6,35 @@
  * Time: 21:59
  */
 
-set_time_limit( 0 );
+set_time_limit(0);
+error_reporting(E_CORE_ERROR);  //屏蔽错误
 
-$dst_ip = 'localhost';
-$dst_port = 11198;
+$dst_ip = 'localhost';  //ip
+$dst_port = 11197;  //port
 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if(!$socket)
-{
-    echo 'create error:'.socket_strerror(socket_last_error());
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);  //tcp连接
+if (!$socket) {
+    echo 'create error:' . socket_strerror(socket_last_error($socket));  //创建socket
+    exit;
 }
+
 $bind = socket_bind($socket, $dst_ip, $dst_port);
-if(!$bind)
-{
-    echo 'bind error:'.socket_strerror(socket_last_error());
+if (!$bind) {
+    echo 'bind error:' . socket_strerror(socket_last_error($socket));  //绑定
+    exit;
 }
 
-//socket_listen($socket);
-//$acpt = socket_accept($socket);
-while(true)
-{
-    $connection = socket_listen($socket);
-    if(!$connection)
-    {
-        continue;
+while (true) {
+    $hear = socket_read($socket, 1024);  //读取client发送的信息
+    if ($hear) {
+        echo 'accept: ' . $hear . '</br>';
     }
 
-    $acpt = socket_accept($socket);
-    $hear=socket_read($acpt,1024);
-    echo 'accept: '.$hear.'</br>';
-
-    $words='world3';
-    socket_write($acpt,$words);
-    echo 'send : '.$words;
-
-
-//    usleep( 100000 );
+    if (!empty($hear)) {
+        $words = 'world3';
+        socket_write($socket, $words);  //发送信息到client
+        echo 'send : ' . $words;
+    }
+    usleep(100000);  //避免频繁死循环死机，sleep
 }
 socket_close($socket);
